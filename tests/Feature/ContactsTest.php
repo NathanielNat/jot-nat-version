10dc9bd37dc3a541d1cf19a2253a71cded1220f8
NathanielNat <psalmnat@gmail.com>
2020-09-24 00:01:17 +0000

    unit tests done edsignn next

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -36,11 +36,15 @@ class ContactsTest extends TestCase
       $response = $this->get('/api/contacts?api_token='. $user->api_token);
 
       $response->assertJsonCount(1)
-                ->assertJson([
-                  'data' => [
-                  ['contact_id' => $contact->id]
+      ->assertJson([
+          'data' => [
+              [
+                  "data" => [
+                      'contact_id' => $contact->id
                   ]
-                  ]);
+              ]
+          ]
+      ]);
   }
 
    /** @test */
