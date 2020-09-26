1a0ea2248f80b0a220b87323f250a37b360d493e
NathanielNat <psalmnat@gmail.com>
2020-09-26 12:57:00 +0000

    registeration, login done

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+Route::get('/logout-manual',function(){
+    request()->session()->invalidate();
+});
+
 
 // gets any route and sends to the app controller 
 Route::get('/{any}', 'AppController@index')->where('any','.*');
